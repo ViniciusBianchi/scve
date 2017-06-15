bf67b9d47864cf0eb46db535625f5e30bf5fb18e
Vinicius Bianchi <bianchijobs@gmail.com>
2017-06-15 10:40:46 -0300

    Subindo a versão inicial do projeto.

---
+++ SCVE/SCVE/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SCVE
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
